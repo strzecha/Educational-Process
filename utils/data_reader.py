73f5a76e97955633cfb17d460be12b064b0189e5
strzecha <matteuszdr@gmail.com>
2023-09-27 10:03:02 +0200

    Add MailSender

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -3,6 +3,6 @@ from jproperties import Properties
 def get_properties(file_name="properties"):
     configs = Properties()
     with open(file_name, 'rb') as config_file:
-        configs.load(config_file)
+        configs.load(config_file, encoding="utf-8")
 
     return configs
---
+++ utils/mail_sender.py
@@ -0,0 +1,51 @@
+from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+import ssl
+
+from utils.data_reader import get_properties
+
+properties = get_properties()
+credentials_filename = properties.get("CREDENTIALS_FILE").data
+credentials = get_properties(credentials_filename)
+
+EMAIL_SENDER = credentials.get("EMAIL_SENDER").data
+EMAIL_PASSWORD = credentials.get("EMAIL_PASSWORD").data
+EMAIL_RECEIVER = credentials.get("EMAIL_RECEIVER").data
+USERNAME = credentials.get("USERNAME").data
+
+class MailSender:
+    def __init__(self):
+        self.email_sender = EMAIL_SENDER
+        self.email_password = EMAIL_PASSWORD
+        self.email_receiver = EMAIL_RECEIVER
+        self.username = USERNAME
+
+    def create_mail(self):
+        subject = "Test"
+        body = f"""
+                <!doctype html>
+                <html lang="pl">
+                <head></head>
+                <body>
+                    <h1> Hello </h1>
+                    <p> Scores of {self.username}</p>
+                </body>
+                </html>"""
+
+        self.mail = MIMEMultipart("alternative")
+
+        self.mail['From'] = self.email_sender
+        self.mail['To'] = self.email_receiver
+        self.mail['Subject'] = subject
+        part = MIMEText(body, 'html')
+
+        self.mail.attach(part)
+
+    def send_mail(self):
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(self.email_sender, self.email_password)
+            smtp.sendmail(self.email_sender, self.email_receiver, self.mail.as_string())
