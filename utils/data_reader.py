48bf2d6449d756669044bdd3edcafa1f05d29778
strzecha <matteuszdr@gmail.com>
2023-09-30 11:40:03 +0200

    Add comments to data_reader.py

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -3,6 +3,15 @@ import csv
 from jproperties import Properties
 
 def get_properties(file_name="properties"):
+    """Function to get properties from properties file
+
+    Args:
+        file_name (str, optional): name od properties file. Defaults to "properties".
+
+    Returns:
+        Properties: properties 
+    """
+
     configs = Properties()
     with open(file_name, 'rb') as config_file:
         configs.load(config_file, encoding="utf-8")
@@ -10,6 +19,14 @@ def get_properties(file_name="properties"):
     return configs
 
 def read_data_words_from_file(filename):
+    """Function to read data from csv file
+
+    Args:
+        filename (str): name of csv file
+
+    Returns:
+        list: data from file
+    """
     results = list()
     with open(filename, newline='', encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';', fieldnames=["word", "translation"])
