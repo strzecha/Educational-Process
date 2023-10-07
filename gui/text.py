bb396a8717f32d1671f3a7cb11f43493c40028b4
strzecha <matteuszdr@gmail.com>
2023-10-07 18:49:21 +0200

    Add comments to text.py

--- gui/text.py
+++ gui/text.py
@@ -1,7 +1,24 @@
 import pygame
 
 class Text:
+    """Text class
+    
+    Class to representation of text field in interface
+    """
+
     def __init__(self, text, color, hover_color=None, font_name="Corbel", font_size=20, pos_x=0, pos_y=0):
+        """Init method
+
+        Args:
+            text (str): contents of text field
+            color (tuple): color of text
+            hover_color (tuple, optional): hover color of text. Defaults to None.
+            font_name (str, optional): name of font of text. Defaults to "Corbel".
+            font_size (int, optional): size of font. Defaults to 20.
+            pos_x (int, optional): x position of text field. Defaults to 0.
+            pos_y (int, optional): y position of text field. Defaults to 0.
+        """
+
         self.color = color
         self.hover_color = color if not hover_color else hover_color
 
@@ -12,13 +29,32 @@ class Text:
         self.set_pos(pos_x, pos_y)
 
     def set_pos(self, pos_x, pos_y):
+        """Pos setter
+
+        Args:
+            pos_x (int): x position of text field
+            pos_y (int): y position of text field
+        """
+
         self.pos = (pos_x, pos_y)
 
     def update(self):
+        """Update method
+        """
+        
         self.text_rendered = self.font.render(self.text, True, self.color)
 
     def draw(self, screen):
+        """Method to draw text field on screen
+        
+        Args:
+            screen (pygame.display): interface
+        """
+
         screen.blit(self.text_rendered, self.pos)
 
     def set_text(self, text):
+        """Text setter
+        """
+        
         self.text = text
